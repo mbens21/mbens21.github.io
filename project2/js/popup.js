ffef0892acc2625b86c217ed322ab375541856f1
Meagan Bens <mbens@nd.edu>
2019-03-05 21:26:01 -0500

    wireframe

---
+++ project2/js/popup.js
@@ -0,0 +1,36 @@
+/*basic */
+$(document).ready(function(){ 
+
+
+  $("#indiana").click(function(){
+        $("#modal-indiana").toggleClass("visible");
+        });
+    
+    
+});
+
+$(document).ready(function(){ 
+
+
+  $("#close-indiana").click(function(){
+        $("#modal-indiana").toggleClass("visible");
+        });
+    
+    
+});
+
+$(document).ready(function(){ 
+
+
+  $("#illinois").click(function(){
+        $("#modal-illinios").toggleClass("visible");
+        });
+    
+    $(document).ready(function(){ 
+
+
+  $("#close-illinois").click(function(){
+        $("#modal-illinois").toggleClass("visible");
+        });
+        
+        
